72d1cd3e74a822c8cfb8e1a5305b676d6fe3a5b5
dibley1973 <dibley1973@yahoo.co.uk>
2016-07-26 06:28:41 +0100

    20160726_0626_DWDT Added test  harness for package load

---
+++ Tests/Dibware.EventDispatcher.Core.PackageLoader/Events/SimpleEvent1.cs
@@ -0,0 +1,8 @@
+﻿using Dibware.EventDispatcher.Core.Contracts;
+
+namespace Dibware.EventDispatcher.Core.PackageLoader.Events
+{
+    internal class SimpleEvent1 : IApplicationEvent
+    {
+    }
+}
---
+++ Tests/Dibware.EventDispatcher.Core.PackageLoader/Program.cs
@@ -0,0 +1,31 @@
+﻿using Dibware.EventDispatcher.Core.Contracts;
+using Dibware.EventDispatcher.Core.PackageLoader.Events;
+using System;
+
+namespace Dibware.EventDispatcher.Core.PackageLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARRANGE
+            bool handlerCalled = false;
+            ApplicationEventHandlerDelegate<SimpleEvent1> @delegate =
+                delegate
+                {
+                    handlerCalled = true;
+                };
+            var dispatcher = new ApplicationEventDispatcher();
+            dispatcher.AddListener(@delegate);
+
+            // ACT
+            dispatcher.Dispatch(new SimpleEvent1());
+            dispatcher.Dispose();
+
+            // ASSERT
+            Console.WriteLine("Handler called: {0}", handlerCalled);
+
+            Console.ReadKey();
+        }
+    }
+}
